4ad1da0228381b2bcb2bd091655a9d90a90be969
Samuel Serra <sserradev@gmail.com>
2022-04-26 11:22:03 -0300

    update

--- public/index.js
+++ public/index.js
@@ -10,5 +10,9 @@ sr.reveal('.home-text a', { delay: 550 })
 
 sr.reveal('div.main img', { delay: 500 })
 
-sr.reveal('div.top', { origin: 'top' })
-sr.reveal('div.bottom', { delay: 500, origin: 'top' })
+sr.reveal('div.top', { origin: 'top', duration: 900 })
+sr.reveal('div.bottom', { duration: 900, origin: 'top', delay: 500 })
+
+sr.reveal('.text-long', { origin: 'left',delay: 100, duration: 700 })
+sr.reveal('.text-long p', { origin: 'left', duration: 700 })
+sr.reveal('main img', { origin: 'left', delay: 200, duration: 700 })
