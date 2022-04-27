eabbea5bc59912e6e8af064daa5de9d737453cac
Samuel Serra <sserradev@gmail.com>
2022-04-27 10:47:23 -0300

    update

--- public/JS/index.js
+++ public/JS/index.js
@@ -5,7 +5,7 @@ const sr = ScrollReveal({
     delay: 400
 })
 
-sr.reveal('.home-text h1, p')
+sr.reveal('.home-text h1, .home-text p')
 sr.reveal('.home-text a', { delay: 550 })
 
 sr.reveal('div.main img', { delay: 500 })
@@ -16,3 +16,16 @@ sr.reveal('div.bottom', { duration: 900, origin: 'top', delay: 500 })
 sr.reveal('.text-long', { origin: 'left',delay: 100, duration: 700 })
 sr.reveal('.text-long p', { origin: 'left', duration: 700 })
 sr.reveal('main img', { origin: 'left', delay: 200, duration: 700 })
+
+sr.reveal('.serra', { origin: 'top' })
+sr.reveal('.biel', { origin: 'top', delay: 500 })
+sr.reveal('.paolo', { origin: 'top', delay: 600 })
+sr.reveal('.viana', { origin: 'top', delay: 700 })
+sr.reveal('.dede', { origin: 'top', delay: 800 })
+
+
+
+
+
+sr.reveal('footer')
+
